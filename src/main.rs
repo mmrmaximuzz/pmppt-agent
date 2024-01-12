4c9dce1ad6a07d5d4950ffb5b1aadf1c3833192d
Maxim Petrov <mmrmaximuzz@gmail.com>
2024-01-12 23:30:30 +0100

    Add local file control implementation

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,45 @@
 mod agent;
 mod protocol_impl;
 
-fn main_local(_args: &[String]) {
-    unimplemented!()
+/// Little helper function to convert str literals to error message.
+fn emsg(s: &str) -> Result<(), String> {
+    Err(s.into())
 }
 
-fn main_tcp(_args: &[String]) {
-    unimplemented!()
+fn main_local(args: &[String]) -> Result<(), String> {
+    if args.len() != 1 {
+        return emsg("usage: PROG local PATH_TO_CONFIG");
+    }
+
+    let json_path = &args[0];
+
+    let proto = protocol_impl::LocalProtocol::from_json(json_path)?;
+    let agent = agent::Agent::new(proto);
+    agent.serve();
+    Ok(())
+}
+
+fn main_tcp(_args: &[String]) -> Result<(), String> {
+    emsg("tcp transport not implemented")
+}
+
+fn main_wrapper(args: &[String]) -> Result<(), String> {
+    if args.len() < 2 {
+        return emsg("usage: PROG (tcp|local) ARGS...");
+    }
+
+    match args[1].as_str() {
+        "local" => main_local(&args[2..]),
+        "tcp" => main_tcp(&args[2..]),
+        _ => emsg("Only 'tcp' or 'local' transports supported"),
+    }
 }
 
 fn main() {
     // TODO: here will be better CLI arguments parsing
     let args: Vec<String> = std::env::args().collect();
-    if args.len() < 2 {
-        eprintln!("usage: {} (tcp|local) ARGS...", args[0]);
+    if let Err(msg) = main_wrapper(&args) {
+        eprintln!("Error: {}", msg);
         std::process::exit(1);
-    }
-
-    match args[1].as_str() {
-        "local" => return main_local(&args[2..]),
-        "tcp" => return main_tcp(&args[2..]),
-        _ => {
-            eprintln!("Only 'tcp' or 'local' transports supported");
-            std::process::exit(1);
-        }
     }
 }
---
+++ src/protocol_impl.rs
@@ -0,0 +1,64 @@
+//! Implementations of PMPPT protocol for the agent.
+
+use std::fs;
+
+use serde::Deserialize;
+use serde_json::Value;
+
+use crate::agent::protocol;
+
+#[derive(Deserialize)]
+#[serde(tag = "type", content = "data")]
+enum LocalRequest {
+    Poll { path: String },
+    Stop {},
+}
+
+pub struct LocalProtocol {
+    requests: Vec<protocol::PmpptRequest>,
+}
+
+impl LocalProtocol {
+    pub fn from_json(json_path: &str) -> Result<Self, String> {
+        // first read the JSON file completely
+        let content = fs::read_to_string(json_path)
+            .map_err(|e| format!("cannot read '{}' - {}", json_path, e))?;
+
+        // parse as raw JSON list first
+        let values: Vec<Value> =
+            serde_json::from_str(&content).map_err(|e| format!("bad JSON format - {}", e))?;
+
+        // then map every command to PMPPT protocol
+        let mut requests = vec![];
+        for value in values {
+            let request = match serde_json::from_value(value.clone())
+                .map_err(|_| format!("bad/unsupported request {}", value))?
+            {
+                LocalRequest::Poll { path } => {
+                    protocol::PmpptRequest::Poll {
+                        path,
+                    }
+                }
+                LocalRequest::Stop {} => {
+                    protocol::PmpptRequest::Finish {}
+                }
+            };
+            requests.push(request);
+        }
+
+        // reverse to allow consuming commands with .pop() call
+        requests.reverse();
+
+        Ok(LocalProtocol { requests })
+    }
+}
+
+impl protocol::Protocol for LocalProtocol {
+    fn recv_request(&mut self) -> Option<protocol::PmpptRequest> {
+        self.requests.pop()
+    }
+
+    fn send_response(&mut self, _response: protocol::PmpptResponse) -> Option<()> {
+        todo!()
+    }
+}
