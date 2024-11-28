d8ee0f9b745aede89c758e3199b817af715b7201
Maxim Petrov <mmrmaximuzz@gmail.com>
2024-11-28 12:15:30 +0300

    poller: add polling header for postprocessing

--- src/agent/poller.rs
+++ src/agent/poller.rs
@@ -5,6 +5,8 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::time::Duration;
 
+use serde::Serialize;
+
 const DEFAULT_SLEEP_TIME: Duration = Duration::from_millis(250);
 const FILE_CAP: usize = 4 << 10;
 const TOTAL_CAP: usize = 64 << 10;
@@ -13,9 +15,40 @@ pub struct PollConfig {
     sleep_time: Duration,
 }
 
+#[derive(Serialize)]
+struct PollHeader {
+    files: Vec<String>,
+    period: Duration,
+}
+
+fn create_header(files: &[PathBuf], cfg: &PollConfig) -> String {
+    let header = PollHeader {
+        files: files
+            .iter()
+            .cloned()
+            .map(|p| p.to_str().unwrap().to_owned())
+            .collect(),
+        period: cfg.sleep_time,
+    };
+    let mut header = serde_json::to_string(&header).unwrap(); // should never fail
+    header.push('\n'); // insert newline after the header
+    header
+}
+
+fn store_header(output: &mut dyn Write, header: &str) {
+    // dump and flush the poller header first to improve potential diagnostics
+    output
+        .write_all(header.as_bytes())
+        .expect("failed to write header");
+    output
+        .flush()
+        .expect("cannot flush the file after writing header");
+}
+
 pub fn poll_with_config(srcs: Vec<PathBuf>, dest: PathBuf, stop: Arc<AtomicBool>, cfg: PollConfig) {
-    // open destination file with the final content
+    // open destination file with the final content and store header
     let mut output = File::create(dest).expect("cannot open file");
+    store_header(&mut output, &create_header(&srcs, &cfg));
 
     let mut strbuffer = String::new();
     let mut outbuffer = String::new();
